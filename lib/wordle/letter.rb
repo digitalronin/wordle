1590585b6d2469046ed772fd798114f6bd7aa917
digitalronin <david@digitalronin.com>
2022-01-30 13:39:36 +0900

    Add a class to output a char with guess status
    We display characters differently depending on
    whether they're correct (right char and position),
    wrong (char doesn't exist in the target word), or
    wrong_pos (char exists in target word, but not at
    the current position).
    
    This class models that and formats the char for
    output.

---
+++ lib/wordle/letter.rb
@@ -0,0 +1,43 @@
+# Represents a single letter in the result of a wordle guess.
+class Letter
+  attr_reader :char, :status
+
+  def initialize(char, status)
+    @char = char
+    @status = status
+  end
+
+  def formatted
+    case status
+    when :correct
+      correct(char)
+    when :wrong
+      wrong(char)
+    when :wrong_pos
+      wrong_pos(char)
+    else
+      raise "Unknown status #{status} for char #{char}"
+    end
+  end
+
+  private
+
+  def correct(str)
+    # Bold white text on green background
+    coloured(37, 42, str)
+  end
+
+  def wrong(str)
+    # Bold white text on grey background
+    coloured(37, 47, str)
+  end
+
+  def wrong_pos(str)
+    # Bold white text on yellow background
+    coloured(37, 43, str)
+  end
+
+  def coloured(fg, bg, str)
+    "\e[1;#{fg};#{bg}m#{str}\e[0m"
+  end
+end

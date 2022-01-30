6864584354a036832773c9d35cd54f00ba453b77
digitalronin <david@digitalronin.com>
2022-01-30 14:31:57 +0900

    Implement a basic game loop

---
+++ bin/play.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+# Play a round of wordle
+
+require_relative("../lib/wordle")
+
+WORDS_FILE = "data/valid-words.txt"
+
+def random_word(file)
+  File.readlines(file).sample.chomp
+end
+
+secret = random_word(WORDS_FILE)
+
+running = true
+guess_count = 0
+
+while running
+  print "> "
+  input = gets.chomp
+  case input
+  when "q", "quit", "exit"
+    puts "Exiting"
+    running = false
+  when "reveal", "show"
+    puts "Secret word: #{secret}"
+  when /^[a-z]{5}$/
+    guess_count += 1
+    puts Guess.new(input.downcase, secret).formatted
+  end
+end
